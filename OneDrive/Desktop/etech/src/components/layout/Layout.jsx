914ebfedaa67f8dc2a66c0f66e31a3e19acf8d36
Tanuja-Talikhede <talikhedetanuja@gmail.com>
2024-11-26 10:03:54 +0530

    Navbar

--- OneDrive/Desktop/etech/src/components/layout/Layout.jsx
+++ OneDrive/Desktop/etech/src/components/layout/Layout.jsx
@@ -1,11 +1,13 @@
+import LeftPart from "../navbar/LeftPart";
 import NavbarContainer from "../navbar/NavbarContainer";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div>
-      <h1>Layout</h1>
-      <NavbarContainer />
+
+    
+      <NavbarContainer/>
       <Outlet />
     </div>
   );
--- OneDrive/Desktop/etech/src/components/navbar/LeftPart.jsx
+++ OneDrive/Desktop/etech/src/components/navbar/LeftPart.jsx
@@ -1,5 +1,24 @@
+
+import './Navbar.css'
 const LeftPart = () => {
-  return <div>Logo</div>;
+  return (
+    <div class="navbar-left">
+    <a href="#" class="logo">
+      <img src="	https://themes.stackbros.in/eduport-next/assets/logo-22DQx7cW.svg" alt="Logo" class="logo-img"  /> 
+    </a>
+   <a href="" class="category">
+   <button class="category-btn">
+      <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16" class="icon">
+        <circle cx="4" cy="4" r="2"></circle>
+        <circle cx="12" cy="4" r="2"></circle>
+        <circle cx="4" cy="12" r="2"></circle>
+        <circle cx="12" cy="12" r="2"></circle>
+      </svg>
+      Category
+    </button>
+   </a>
+  </div>
+  )
 };
 
 export default LeftPart;
